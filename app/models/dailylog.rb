e78e7beb50d525a522304496221c1fb74bdc2226
beckyphan <beckyphan@live.com>
2020-05-18 16:29:50 -0700

    user associated to nutrients through foods, dailylog has_many foods

--- app/models/dailylog.rb
+++ app/models/dailylog.rb
@@ -1,3 +1,4 @@
 class Dailylog < ApplicationRecord
   belongs_to :user
+  has_many :foods
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :dailylogs
+  has_many :nutrients through: :foods
 end

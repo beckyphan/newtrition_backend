baccedab639c1226acc983d2ab8a0e9d7da7e18c
beckyphan <beckyphan@live.com>
2020-05-18 15:55:39 -0700

    corrected addDobHeightWeightBmiGender migration

--- db/migrate/20200518221057_add_dob_height_weight_bmi_gender_to_user.rb
+++ db/migrate/20200518221057_add_dob_height_weight_bmi_gender_to_user.rb
@@ -1,9 +1,9 @@
 class AddDobHeightWeightBmiGenderToUser < ActiveRecord::Migration[6.0]
   def change
-    t.date :dob
-    t.float :height
-    t.float :weight
-    t.float :bmi
-    t.string :gender
+    add_column :users, :dob, :date
+    add_column :users, :height, :float
+    add_column :users, :weight, :float
+    add_column :users, :bmi, :float
+    add_column :users, :gender, :string
   end
 end

bda1215f1c6ef316d5a83b15f7be33ec253641fc
beckyphan <beckyphan@live.com>
2020-05-18 17:23:57 -0700

    created dailylogs#index route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :foods, only: [:index]
       resources :nutrients, only: [:index]
+      resources :dailylogs, only: [:index]
     end
   end
 end

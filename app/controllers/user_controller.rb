f8e22708d91161762a4198981ae2f015c019705c
beckyphan <beckyphan@live.com>
2020-05-21 16:49:19 -0700

    create user controller and actions

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,21 @@
+class UserController < ApplicationController
+
+  def create
+  end
+
+  def new
+  end 
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end

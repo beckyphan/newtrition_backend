09b031d989c2d1f3609bd01d696572ac37468d9e
beckyphan <beckyphan@live.com>
2020-05-21 16:41:41 -0700

    updated nutrients controller action with NutritentSerializer

--- app/controllers/api/v1/nutrients_controller.rb
+++ app/controllers/api/v1/nutrients_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::NutrientsController < ApplicationController
 
   def index
     nutrients = Nutrient.all
-    render json: nutrients
+    render json: NutrientSerializer.new(nutrients)
   end
 
 end

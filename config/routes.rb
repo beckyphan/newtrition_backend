69f29d75724c2769b6add20a7ce43821a7d07bc4
beckyphan <beckyphan@live.com>
2020-05-21 16:50:08 -0700

    routes for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:create, :update, :show, :destroy]
+
   namespace :api do
     namespace :v1 do
       resources :foods, only: [:index]

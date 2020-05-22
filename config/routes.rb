775005f3f6a31fb359cbd13ae6d3d5207d69a242
beckyphan <beckyphan@live.com>
2020-05-21 17:00:07 -0700

    updated routes to include sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :update, :show, :destroy]
 
+  get '/login', to: 'sessions#login'
+  get '/logout', to: 'sessions#logout'
+
   namespace :api do
     namespace :v1 do
       resources :foods, only: [:index]

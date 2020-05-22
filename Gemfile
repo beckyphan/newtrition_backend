5b21a7971ae06ce491166b269b4bc98435a2089a
beckyphan <github email address>
2020-05-22 09:19:17 -0700

    updated gemfile to include byebug, updated invalid status in users_controller

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'fast_jsonapi'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'byebug'
   gem 'pry'
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,14 @@ class UsersController < ApplicationController
     new_user = User.new(username: params[:username], password: params[:password])
     existing_user = User.find_by(username: params[:username])
 
-    if new_user.save!
+    if new_user.save
       session[:user_id] = new_user.id
       render json: UserSerializer.new(new_user), status: :accepted
     elsif existing_user && existing_user.authenticate(params[:password])
       session[:user_id] = existing_user.id
-      render json: {}, status: :accepted
+      render json: UserSerializer.new(existing_user), status: :accepted
     else
-      render json: {}, status: :invalid_login
+      render json: {}, status: :unauthorized
     end
 
   end

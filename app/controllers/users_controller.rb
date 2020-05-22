0368c4dd2cc3b84f5356db74c7932eb4c5295c86
beckyphan <beckyphan@live.com>
2020-05-21 18:59:15 -0700

    updated user model - validations, updated users controller for login/created new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
 class UsersController < ApplicationController
 
-  def index
-    users = User.all
-    render json: UserSerializer.new(users)
+  def create
+    new_user = User.new(username: params[:username], password: params[:password])
+    existing_user = User.find_by(username: params[:username])
+
+    if new_user.save!
+      session[:user_id] = new_user.id
+      render json: UserSerializer.new(new_user), status: :accepted
+    elsif existing_user && existing_user.authenticate(params[:password])
+      session[:user_id] = existing_user.id
+      render json: {}, status: :accepted
+    else
+      render json: {}, status: :invalid_login
+    end
+
   end
+
+  private
+  def user_params
+    params.permit(:username, :password)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  validates_presence_of :username, :password
+  validates_uniqueness_of :username
+  
   has_many :dailylogs
   has_many :nutrients, through: :foods
 end

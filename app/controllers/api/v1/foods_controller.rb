f5a789322384d203cacb6b39962ea08a1bceb675
beckyphan <github email address>
2020-05-22 20:38:52 -0700

    added food#create action, added validations to food model

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -5,4 +5,15 @@ class Api::V1::FoodsController < ApplicationController
     render json: FoodSerializer.new(foods)
   end
 
+  def create
+    food = Food.create(food_params)
+    render json: FoodSerializer.new(food)
+  end
+
+  private
+
+  def food_params
+    params.permit(:name, :serving, :size, :weight, :calories, :fat, :sodium, :potassium, :carbohydrates, :protein, :vitaminA, :vitaminC, :calcium, :iron, :dailylog_id) 
+  end
+
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,4 @@
 class Food < ApplicationRecord
     belongs_to :dailylog
+    validates_presence_of :name, :serving
 end

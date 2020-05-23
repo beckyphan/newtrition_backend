1cbdf492cb1e61a2ff01b1651420ab7c7616de57
beckyphan <github email address>
2020-05-22 20:34:03 -0700

    added nutrientprofile#create action, added validations to nutrientprofile model

--- app/controllers/api/v1/nutrientprofiles_controller.rb
+++ app/controllers/api/v1/nutrientprofiles_controller.rb
@@ -5,4 +5,15 @@ class Api::V1::NutrientprofilesController < ApplicationController
     render json: NutrientprofileSerializer.new(nutrientprofiles)
   end
 
+  def create
+    nutrientprofile = Nutrientprofile.create(nutrientprofile_params)
+    render json: NutrientprofileSerializer.new(nutrientprofile), status: :accepted
+  end
+
+  private
+
+  def nutrientprofile_params
+    params.permit(:name, :serving, :size, :weight, :calories, :fat, :sodium, :potassium, :carbohydrates, :protein, :vitaminA, :vitaminC, :calcium, :iron)
+  end
+
 end
--- app/models/nutrientprofile.rb
+++ app/models/nutrientprofile.rb
@@ -1,3 +1,4 @@
 class Nutrientprofile < ApplicationRecord
-
+  validates_presence_of :name, :serving, :size, :weight
+  validates_uniqueness_of :name
 end

b9882117f3b342f3a3b58196cea3ff541b4bbef3
beckyphan <github email address>
2020-05-22 20:27:44 -0700

    updated dri#create action

--- app/controllers/dris_controller.rb
+++ app/controllers/dris_controller.rb
@@ -1,7 +1,17 @@
 class DrisController < ApplicationController
   def index
+    dris = Dri.all
+    render json: DriSerializer.new(dris)
   end
 
   def create
+    dri = Dri.create(dri_params)
+    render json: DriSerializer.new(dri), status: :accepted
+  end
+
+  private
+
+  def dri_params
+    params.permit(:calories, :fat, :sodium, :potassium, :carbohydrates, :protein, :vitaminA, :vitaminC, :calcium, :iron)
   end
 end

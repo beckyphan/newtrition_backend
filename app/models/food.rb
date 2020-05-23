c492460d27becd09e85b763a708394d73423c365
beckyphan <github email address>
2020-05-22 21:35:39 -0700

    comment with potential foodtemplate aka nutrientprofile validation for food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,13 @@
 class Food < ApplicationRecord
     belongs_to :dailylog
     validates_presence_of :name, :serving
+    # validate :food_with_existing_nutrientprofile
+    #
+    # def food_with_existing_nutrientprofile
+    #   existing = Nutrientprofile.find_by(name: name)
+    #
+    #   if !existing
+    #     errors.add(:name, "is not yet added to nutrientprofile database")
+    #   end
+    # end
 end
